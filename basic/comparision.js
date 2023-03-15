5383c1a5daa2c18e028c0e2adfa974298f099c22
prasanj33t <127778068+prasanj33t@users.noreply.github.com>
2023-03-15 09:58:30 +0000

    comparison

---
+++ basic/comparision.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1); // simple comparision
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null <= 0); // it's true because comparision convert null to a number, treating as 0 thats why null>=0 is true
+// and mull> 0 is false
+
+console.log(undefined == 0);
+
+// we try to avoid these codes and try to write clean code
+
--- basic/conversionOperation.js
+++ basic/conversionOperation.js
@@ -24,5 +24,36 @@ let booleanisLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// operation //
+ let value  = 3
+ let negValue = -value
+ // console.log(negValue);
+
+let str1 = "hello"
+let str2 = " Prasanjeet"
+
+let str3 = str1 + str2
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 //we domt use these type of because of redability
+
+let gameCounter = 100
+// gameCounter++;
+++gameCounter; //both are same (read prefix and postfix js mdn)
+
+
+console.log(gameCounter);
+
